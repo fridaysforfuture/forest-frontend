503c9b620aec9340f283d2530bbdeed1053d948a
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-06-16 18:00:55 +0200

    Now with actual working create page
    The create page now actually works. But the api url is fixed at the
    moment

--- pages/create.vue
+++ pages/create.vue
@@ -1,42 +1,45 @@
 <template>
-  <section class="section">
-    <form>
-      <b-field
-        label="Name">
-        <b-input
-          v-model="name"
-          required />
-      </b-field>
-      <b-field label="Freundlicher Name">
-        <b-input
-          v-model="friendlyName"
-          required />
-      </b-field>
-      <section class="section">
-        <label>
-          Links:
-        </label>
-        <b-field v-for="link of links">
-          <b-field label="Text">
-            <b-input
-              v-model="link.text"/>
-          </b-field>
-          <b-field label="URL">
-            <b-input
-              v-model="link.url"/>
-          </b-field>
+  <form
+    @submit.prevent="submit">
+    <b-field
+      label="Name">
+      <b-input
+        v-model="name"
+        required />
+    </b-field>
+    <b-field label="Freundlicher Name">
+      <b-input
+        v-model="friendlyName"
+        required />
+    </b-field>
+    <section class="section">
+      <label>
+        Links:
+      </label>
+      <b-field v-for="link of links">
+        <b-field label="Text">
+          <b-input
+            v-model="link.text"/>
         </b-field>
-      </section>
+        <b-field label="URL">
+          <b-input
+            v-model="link.url"/>
+        </b-field>
+      </b-field>
       <b-button
         @click="addLink">
         Neuer Link
       </b-button>
-    </form>
-  </section>
+    </section>
+    <b-button native-type="submit">
+      Eintrag erstellen
+    </b-button>
+  </form>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-class-decorator';
+import axios from 'axios';
 
 @Component 
 export default class CreateView extends Vue {
@@ -52,6 +55,14 @@ export default class CreateView extends Vue {
       text: '',
       url: ''
     });
+  }
+
+  submit() {
+    axios.put(`http://localhost:3001/entries/${this.name}`,
+      {
+        friendlyName: this.friendlyName,
+        links: this.links
+      });
   }
 }
 </script>
