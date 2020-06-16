51c1370be16114974fd195df5e10ddfef00ebbe4
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-06-16 17:50:14 +0200

    Actual fields are finished
    The fields still look pretty ugly, but everything is pretty much usuable
    
    It does not work yet, but theoretically.

--- pages/create.vue
+++ pages/create.vue
@@ -3,23 +3,55 @@
     <form>
       <b-field
         label="Name">
-        <b-input />
+        <b-input
+          v-model="name"
+          required />
       </b-field>
-      <b-field
-        label="Freundlicher Name">
-        <b-input />
+      <b-field label="Freundlicher Name">
+        <b-input
+          v-model="friendlyName"
+          required />
       </b-field>
-      <b-field
-        label="Links">
-        <section class="section">
-          <b-field label="Name">
-            <b-input />
+      <section class="section">
+        <label>
+          Links:
+        </label>
+        <b-field v-for="link of links">
+          <b-field label="Text">
+            <b-input
+              v-model="link.text"/>
           </b-field>
           <b-field label="URL">
-            <b-input />
+            <b-input
+              v-model="link.url"/>
           </b-field>
-        </section>
-      </b-field>
+        </b-field>
+      </section>
+      <b-button
+        @click="addLink">
+        Neuer Link
+      </b-button>
     </form>
   </section>
 </template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-class-decorator';
+
+@Component 
+export default class CreateView extends Vue {
+  name = '';
+  friendlyName = '';
+  links = [{
+    text: '',
+    url: ''
+  }];
+
+  addLink() {
+    this.links.push({
+      text: '',
+      url: ''
+    });
+  }
+}
+</script>
