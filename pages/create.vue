93e0d7b98e784682d6a308c6ffd5d02cadb445b8
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-06-16 17:19:25 +0200

    First create formular

---
+++ pages/create.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="section">
+    <form>
+      <b-field
+        label="Name">
+        <b-input />
+      </b-field>
+      <b-field
+        label="Freundlicher Name">
+        <b-input />
+      </b-field>
+      <b-field
+        label="Links">
+        <section class="section">
+          <b-field label="Name">
+            <b-input />
+          </b-field>
+          <b-field label="URL">
+            <b-input />
+          </b-field>
+        </section>
+      </b-field>
+    </form>
+  </section>
+</template>
