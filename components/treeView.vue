e71a1c728682a041f9868b4102eb70e628585d6b
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-06-19 14:40:31 +0200

    Create a tree view to make it modular

---
+++ components/treeView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <b-field
+      label="Name">
+      <b-input
+        v-model="name"
+        required />
+    </b-field>
+    <b-field label="Freundlicher Name">
+      <b-input
+        v-model="friendlyName"
+        required />
+    </b-field>
+    <section class="section">
+      <label>
+        Links:
+      </label>
+      <b-field v-for="(link, i) of links" :v-key="i">
+        <b-field label="Text">
+          <b-input v-model="link.text"/>
+        </b-field>
+        <b-field label="URL">
+          <b-input
+            v-model="link.url"/>
+        </b-field>
+      </b-field>
+      <b-button @click="addLink">
+        Neuer Link
+      </b-button>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import axios from 'axios';
+
+@Component 
+export default class TreeView extends Vue {
+  @Prop(Object) value!: any;
+
+  get name() {
+    return this.value.name;
+  }
+  set name(newName) {
+    this.value.name = newName;
+  }
+  get friendlyName() {
+    return this.value.friendlyName;
+  }
+  set friendlyName(newFriendlyName) {
+    this.value.friendlyName = newFriendlyName;
+  }
+  get links() {
+    return this.value.links;
+  }
+  set links(newLinks) {
+    this.value.links = newLinks;
+  }
+
+  addLink() {
+    this.links.push({
+      text: '',
+      url: ''
+    });
+  }
+}
+</script>
---
+++ pages/edit/_name.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="section">
+    <tree-view v-if="entry !== null" v-model="entry">
+    </tree-view>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import TreeView from '@/components/treeView.vue';
+import axios from 'axios';
+
+@Component({
+  components: {
+    TreeView,
+  }
+})
+export default class CreateView extends Vue {
+  entry = null;
+
+  async created() {
+    const response = await
+      this.$axios.get(`http://localhost:3001/entries/${this.$route.params.name}`);
+    this.entry = response.data;
+  }
+}
+</script>
