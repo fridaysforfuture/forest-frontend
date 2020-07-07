54dc3cafc2a1ad2546089b2a5485094388f29316
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-07-07 19:31:51 +0200

    Added proper login page

---
+++ pages/login.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="title has-text-centered"> Du wirst gleich weitergeleitet </h1>
+    <b-loading active :is-full-page="false">
+    </b-loading>
+  </div>
+</template>
